d27794307a9b7abb173bba77fce2ab445e84b7e0
monguri <mongry@gmail.com>
2015-10-17 18:40:23 +0900

    点を描画する処理を追加した

--- MGRRenderer/main.cpp
+++ MGRRenderer/main.cpp
@@ -2,21 +2,23 @@
 #include <assert.h>
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
+#include <vector>
 
 typedef struct {
 	GLuint vertexShader;
 	GLuint fragmentShader;
 	GLuint shaderProgram;
-	// ここより下は、形状によって別クラスに保存するとよい
+	// 点、線、ポリゴンの範囲ならこのデータだけで大丈夫
 	GLint attributePosition;
+	GLint attributePointSize;
 } RenderingContext;
 
 // 宣言
 static void fwErrorHandler(int error, const char* description);
 static void fwKeyInputHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
-static void initialize(RenderingContext& context);
-static void render(RenderingContext& context);
-static void finalize(RenderingContext& context);
+static void initialize(std::vector<RenderingContext>& contexts);
+static void render(const std::vector<RenderingContext>& contexts);
+static void finalize(const std::vector<RenderingContext>& contexts);
 
 int main()
 {
@@ -53,15 +55,15 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	RenderingContext context;
+	std::vector<RenderingContext> contexts;
 
 	// 描画の初期化
-	initialize(context);
+	initialize(contexts);
 
 	while (glfwWindowShouldClose(window) == GL_FALSE)
 	{
 		// 描画のメインループ
-		render(context);
+		render(contexts);
 
 		glfwSwapBuffers(window);
 
@@ -69,7 +71,7 @@ int main()
 	}
 
 	// 描画の終了処理
-	finalize(context);
+	finalize(contexts);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
@@ -90,15 +92,80 @@ void fwKeyInputHandler(GLFWwindow* window, int key, int scancode, int action, in
 }
 
 // TODO:リサイズイベントを受けるメソッドもつくりたいな
+static void initializePoint(RenderingContext& context);
+static void initializeLine(RenderingContext& context);
+static void renderPoint(const RenderingContext& context);
+static void renderLine(const RenderingContext& context);
+
 static GLuint createVertexShader(const GLchar* source);
 static GLuint createFragmentShader(const GLchar* source);
 static GLuint createShaderProgram(const GLuint vertexShader, const GLuint fragmentShader);
 static void destroyShaderProgram(const GLuint program, const GLuint vertexShader, const GLuint fragmentShader);
 
 // 描画物のデータ構造と、OpenGLを使って描画するロジックは切り離して書くべき。OpenGL以外に移植するためにね。
-void initialize(RenderingContext& context)
+void initialize(std::vector<RenderingContext>& contexts)
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	RenderingContext pointContext;
+	initializePoint(pointContext);
+	contexts.emplace_back(pointContext);
+
+	RenderingContext lineContext;
+	initializeLine(lineContext);
+	contexts.emplace_back(lineContext);
+
+	//RenderingContext polygonContext;
+	//contexts.push_back(polygonContext);
+
+}
+
+void render(const std::vector<RenderingContext>& contexts)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	renderPoint(contexts[0]);
+
+	renderLine(contexts[1]);
+}
+
+void finalize(const std::vector<RenderingContext>& contexts) {
+	for (const RenderingContext& context : contexts)
+	{
+		destroyShaderProgram(context.shaderProgram, context.vertexShader, context.fragmentShader);
+	}
+}
+
+void initializePoint(RenderingContext& context)
+{
+	context.vertexShader = createVertexShader(
+		"attribute mediump vec4 attr_pos;"
+		"attribute mediump float attr_point_size;"
+		"void main()"
+		"{"
+		"	gl_Position = attr_pos;"
+		"	gl_PointSize = attr_point_size;"
+		"}"
+		);
+	context.fragmentShader = createFragmentShader(
+		"void main()"
+		"{"
+		"	gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);"
+		"}"
+		);
+	context.shaderProgram = createShaderProgram(context.vertexShader, context.fragmentShader);
+
+	context.attributePosition = glGetAttribLocation(context.shaderProgram, "attr_pos");
+	assert(glGetError() == GL_NO_ERROR);
+	assert(context.attributePosition >= 0);
+
+	context.attributePointSize = glGetAttribLocation(context.shaderProgram, "attr_point_size");
+	assert(glGetError() == GL_NO_ERROR);
+	assert(context.attributePointSize >= 0);
+}
+
+void initializeLine(RenderingContext& context)
+{
 	context.vertexShader = createVertexShader(
 		"attribute mediump vec4 attr_pos;"
 		"void main()"
@@ -115,23 +182,49 @@ void initialize(RenderingContext& context)
 	context.shaderProgram = createShaderProgram(context.vertexShader, context.fragmentShader);
 
 	context.attributePosition = glGetAttribLocation(context.shaderProgram, "attr_pos");
-	GLenum err = glGetError();
-	assert(err == GL_NO_ERROR);
+	assert(glGetError() == GL_NO_ERROR);
 	assert(context.attributePosition >= 0);
+}
 
+void renderPoint(const RenderingContext& context)
+{
 	glUseProgram(context.shaderProgram);
+	assert(glGetError() == GL_NO_ERROR);
+
+	glEnableVertexAttribArray(context.attributePosition);
+	assert(glGetError() == GL_NO_ERROR);
+	glEnableVertexAttribArray(context.attributePointSize);
+	assert(glGetError() == GL_NO_ERROR);
+
+	// 本来、これはノードのデータとしてノードを作った段階であるはず
+	// 毎フレーム作るものではない。（staticなので毎回は作られてないけど）
+	static const GLfloat positionAndPointSize[] = {
+		// 座標x, 座標y, ポイントサイズ
+		0.0f, 0.0f, 15.0f,
+		0.75f, 0.75f, 15.0f,
+	};
+
+	glVertexAttribPointer(context.attributePosition, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, (GLvoid*)positionAndPointSize);
+	assert(glGetError() == GL_NO_ERROR);
+	glVertexAttribPointer(context.attributePointSize, 1, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, (GLvoid*)&positionAndPointSize[2]);
+	assert(glGetError() == GL_NO_ERROR);
+
+	glDrawArrays(GL_POINTS, 0, 2);
 	assert(glGetError() == GL_NO_ERROR);
 }
 
-void render(RenderingContext& context)
+void renderLine(const RenderingContext& context)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(context.shaderProgram);
+	assert(glGetError() == GL_NO_ERROR);
 
 	glEnableVertexAttribArray(context.attributePosition);
 	assert(glGetError() == GL_NO_ERROR);
 	glLineWidth(1.0f);
 	assert(glGetError() == GL_NO_ERROR);
 
+	// 本来、これはノードのデータとしてノードを作った段階であるはず
+	// 毎フレーム作るものではない。（staticなので毎回は作られてないけど）
 	static const GLfloat position[] = {
 		-1.0f, 0.0f,
 		1.0f, 0.0f,
@@ -143,10 +236,6 @@ void render(RenderingContext& context)
 	assert(glGetError() == GL_NO_ERROR);
 	glDrawArrays(GL_LINES, 0, 4);
 	assert(glGetError() == GL_NO_ERROR);
-}
-
-void finalize(RenderingContext& context) {
-	destroyShaderProgram(context.shaderProgram, context.vertexShader, context.fragmentShader);
 }
 
 GLuint createVertexShader(const GLchar* source)
